da0a9a7519b76ccdf16a93563a6c10f6c923d80b
DannaCb5 <dannal@bellsouth.net>
2019-01-30 16:25:15 -0500

    Wednesday

--- BullsAndCows.js
+++ BullsAndCows.js
@@ -1,16 +1,52 @@
-let secretNumber= Math.round(((Math.random () * 99999)+9999));
+"use strict"
+
+let secretNumber= Math.round((Math.random ()*10000) + 9999);
 
 console.log("The Secret Number is " + secretNumber);
-let guess=prompt("Guess a 5 digit number");
+let guess=prompt("Guess the first digit of your 5 digit number");
 
-if (guess<10000 || guess>99999) {
-    alert("Guess must be 5 digits and a real number") }
-    else { 
-        alert("Your first guess is " + guess);
+console.log(guess); 
+
+let guesses=0
+let turn = "second"
+while (guesses <= 4) { 
+guess=prompt("Guess the " + turn + " digit of your 5 digit number");
+    if (parseInt(guess) > 9) { console.log("line 14");
+        console.log("Guess must be 1 digit whole number between 0 and 9, your number was" + guess);
+        break;
+    } else if (guesses == 0) { console.log("line 17");
+        let turn = "third"; console.log("line 18")
+        let digit1 = guess; console.log("line 19");
+        console.log("Your first digit is " + digit1);
+        guesses = guesses + 1; console.log("line 21 incremented to " + guesses);
+    } else if (guesses == 1) { console.log("line 22");
+        let turn = "forth"; console.log("line 23")
+        let digit2 = guess; console.log("line 24");
+        console.log("Your second digit is " + digit2); console.log("line 25")
+        guesses = guesses + 1; console.log("line 26 incremented to " + guesses);
+    } else if (guesses == 2) { console.log("line 27");
+        let turn = "fifth"; console.log("line 28")
+        let digit3 = guess; console.log("line 29");
+        console.log("Your third digit is " + digit3); console.log("line 30")
+        guesses = guesses + 1; console.log("line 31 incremented to " + guesses);
+    } else if (guesses == 3) { console.log("line 32");
+        let digit4 = guess; console.log("line 33");
+        console.log("line 34 Your forth digit is " + digit4);
+        guesses = guesses + 1; console.log("line 35 incremented to " + guesses);
+    } else if (guesses == 4) { console.log("line 36 else statement");
+        let digit5 = guess; console.log("line 37 let digit5 statement");
+        console.log("Your fifth digit is " + digit5); console.log("line 38")
+        guesses = guesses + 1; console.log("incremented to " + guesses); console.log("line 39")
+    } else { 
+        console.log(digit1, digit2, digit3, digit4, digit5,  + "line 40");
+    }
+
+
 }
-console.log(Array.from(guess.toString()));
 
-var partsOfStr = str.split(',');
+console.log(Array.from(secretNumber.toString()));
+
+// var partsOfStr = str.split(',');
 
 
 
--- practice.js
+++ practice.js
@@ -1,25 +1,34 @@
 
+let answer= "The answer to this question is "
+for (let i = 0; i<=10; ++i) {
+    if (i % 2 == 0)continue;
+    console.log( i ); 
+    console.log(answer + "2 4 6 8 10" );
+}
 
-console.log(5 > 4);
-console.log("My Guess is: true" );
-console.log("apple" > "pineapple");
-console.log("My Guess is: false" );
-console.log("2" > "12");
-console.log("My Guess is: false" );
-console.log(undefined == null);
-console.log("My Guess is: true" );
-console.log(undefined === null);
-console.log("My Guess is: false" );
-console.log(null == "\n0\n");
-console.log("My Guess is: false" );
-console.log(null === +"\n0\n");
-console.log("My Guess is: false" );
-let a = 1, b = 1;
-let c = ++a; // ?
-let d = b++; // ?
-console.log(a, b, c, d);
-console.log("My Guess is: 1, 1, 2, 1" );
-let e = 2;
-let x = 1 + (e *= 2);
-console.log(e, x);
-console.log("My Guess is: 2, 5");``
+
+
+
+// console.log(5 > 4);
+// console.log("My Guess is: true" );
+// console.log("apple" > "pineapple");
+// console.log("My Guess is: false" );
+// console.log("2" > "12");
+// console.log("My Guess is: false" );
+// console.log(undefined == null);
+// console.log("My Guess is: true" );
+// console.log(undefined === null);
+// console.log("My Guess is: false" );
+// console.log(null == "\n0\n");
+// console.log("My Guess is: false" );
+// console.log(null === +"\n0\n");
+// console.log("My Guess is: false" );
+// let a = 1, b = 1;
+// let c = ++a; // ?
+// let d = b++; // ?
+// console.log(a, b, c, d);
+// console.log("My Guess is: 1, 1, 2, 1" );
+// let e = 2;
+// let x = 1 + (e *= 2);
+// console.log(e, x);
+// console.log("My Guess is: 2, 5");``
